06a3d7f8092ae30404d20edc7e566e9505683303
Sergio Arias <s-andres140@hotmail.com>
2024-03-27 09:41:26 -0500

    hice la pagina de busqueda de pokemons y su filtrado

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,27 @@
+import { useContext } from "react"
+import { PokemonContext } from "../context/PokemonContext"
+import { useLocation } from "react-router-dom"
+import { CardPokemon } from "../components"
 
 export const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
+  const location = useLocation()
+
+  const {globalPokemons} = useContext(PokemonContext)
+  
+   const filteredPokemons = globalPokemons.filter(pokemon => pokemon.name.includes(location.state.toLowerCase()))
+
+
+   return (
+    <div className="container">
+      <p className="p-search">
+        Se encontraron <span>{filteredPokemons.length}</span> resultados:
+      </p>
+      <div className="card-list-pokemon container">
+        {filteredPokemons.map(pokemon => (
+        <CardPokemon pokemon={pokemon} key={pokemon.id}/>
+        
+        ))}
+      </div>
+    </div>
   )
 }
