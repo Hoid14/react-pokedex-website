9d602d2b902822def32b8be7ade7c81286c57b21
Sergio Arias <s-andres140@hotmail.com>
2024-03-27 09:24:02 -0500

    actualizacion componente loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,14 @@
-
+import { dotSpinner } from 'ldrs'
 export const Loader = () => {
+    dotSpinner.register()
   return (
-    <div>Loader</div>
+    <div className="container-loader">
+        <l-dot-spinner
+        size="40"
+        speed="0.9" 
+        color="black" 
+        ></l-dot-spinner>
+    </div>
+    
   )
 }
